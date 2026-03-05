8492fd5283c8bb9a97fd4b54fe47d1440149f346
Prachi Gupta <guptaprachi510@gmail.com>
2026-03-05 10:50:29 +0530

    Complete process

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req,res) =>{
-    res.send("CI/CD set up")
+    res.send("CI/CD set up complete")
 })
 
 app.listen(3000, ()=>{
